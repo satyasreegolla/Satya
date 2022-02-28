d20f20e30e45cabc1aa4bfbfb3d426488a5f697d
Satyasree Golla <sg4cd@umsystem.edu>
2022-02-27 22:28:27 -0600

    Formatting done.

--- Src/Zipcode-example/move-html-to-results.R
+++
@@ -1,6 +0,0 @@
-src <- "~/zipcode-example/src"
-results <- "~/zipcode-example/results"
-for (i in list.files(src, pattern="*.html")) {
-  file.copy(paste(src, i, sep="/"), results)
-  file.remove(paste(src, i, sep="/"))
-}
